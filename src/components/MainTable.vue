b1821a0c236ac8cc0820effd6be3d6dcb68f0288
VRA <vamsheedhar.anduri@agrosolutions.nl>
2024-06-11 16:39:44 +0530

    table component completed

---
+++ src/components/MainTable.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <table class="table table-striped table-hover table-bordered table-responsive">
+      <thead class="table-success">
+        <tr>
+          <th v-for="(heading, index) in Object.keys(tableData[0])" :key="index">{{heading}}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(rowData, rowIndex) in tableData" :key="rowIndex" @click="selectedRow(rowData)">
+          <td v-for="(value, key) in rowData" :key="key">{{ value }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    tableData: Array
+  },
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+    selectedRow(data){
+      this.$emit('selectedRow', data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+//import LoginPage from "../views/pages/LoginPage.vue";
 
 const routes = [
   {
@@ -7,14 +8,19 @@ const routes = [
     name: 'home',
     component: HomeView
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  // {
+  //   path: '/login',
+  //   name: 'Login',
+  //   component: LoginPage
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
 
 const router = createRouter({
