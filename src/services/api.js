07677e8020d5fbf627a1a37e73d84869bab5bfaa
vamseeanduru <vamsee.anduru.com>
2024-08-20 12:17:44 +0530

    added api key to env file

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiKey = '469666465cd8b9f537cf360c1ef82d6d';
+const apiKey = process.env.VUE_APP_API_KEY;
 
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
